43278aa67ab2567481a5455e807cc7044b490a79
Trstnin <152942538+Trstnin@users.noreply.github.com>
2024-12-19 01:49:09 +0000

    add register controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,13 +2,14 @@ import { asynchandlers } from "../utils/asyncHandlers.js";
 import {ApiError} from "../utils/apiError.js" 
 import { User } from "../models/user.model.js"
 import { ApiResponse } from "../utils/apiResponse.js"
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 const registerUser = asynchandlers( async(req , res) => {
  // get user details from frontend
  //validation  - not empty
  //check if user already exists: username and email
  //check for images , check for avatar
- // upload them to cloudinary , avatar
+ // upload them to cloudinary , avatara
  // create user objects - create entry in db
  //remove password and refresh token field from response
  // check for user creation 
@@ -23,7 +24,7 @@ const registerUser = asynchandlers( async(req , res) => {
     throw new ApiError(400 , "All fields are required")
  }
   
-const existedUser = User.findOne({
+const existedUser = await User.findOne({
     $or:[
         { username } , { email }
     ]
@@ -35,14 +36,20 @@ const existedUser = User.findOne({
     }
     
    const avatarLocalPath =  req.files?.avatar[0]?.path;
-   const coverImageLocalPath = req.files?.coverImage[0]?.path
+   //const coverImageLocalPath = req.files?.coverImage[0]?.path
+   let coverImageLocalPath;
+  if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+   coverImageLocalPath = req.files.coverImage[0].path
+  }
 
    if (!avatarLocalPath) {
        throw new ApiError(400 , "Avatar file is required")
    }
 
  const avatar =  await uploadOnCloudinary(avatarLocalPath);
- const coverImage = await uploadOnCloudinary(coverImage);
+ const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+
 
  if(!avatar){
     throw new ApiError(400, "avatar file is required")
@@ -57,7 +64,7 @@ const existedUser = User.findOne({
     username : username.toLowerCase()
 })
 
- createdUser = await User.findById(user._id).select(
+ const createdUser = await User.findById(user._id).select(
     "-password -refreshToken"
  )
 
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,6 @@
 import {Router} from "express"
 import { registerUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middlewares.js";
-import {uploadOnCloudinary } from "../utils/cloudinary.js"
 
 const router = Router();
 
